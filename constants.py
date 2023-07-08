4363f73a4505d3600b0610e7836389d424f7ea6d
Ratiarisandy Dinoh <50234882+DinohRatiarisandy@users.noreply.github.com>
2023-07-08 17:39:45 +0300

    all constant of the game

---
+++ constants.py
@@ -0,0 +1,9 @@
+import pygame
+
+WIDTH = 400
+HEIGHT = 400
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+COLR_TILE = (223, 220, 218)
+BACKGROUND = (26, 31, 40)
+COLR_NUM = (0, 0, 128)
+SPACECING = 1
